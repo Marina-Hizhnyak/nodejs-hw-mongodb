c721e9a4c3f5ab68ed855be78b3431ac58be9c47
Marina-Hizhnyak <maryfadeeva1208@gmail.com>
2025-10-25 09:47:30 +0200

    swagger_done

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import path from 'node:path';
+import * as fs from 'node:fs';
+import swaggerUI from "swagger-ui-express";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import contactsRoutes from "./routes/contacts.js";
@@ -11,12 +13,15 @@ import cookieParser from "cookie-parser";
 import { auth } from './middlewares/authenticate.js';
 
 export function setupServer() {
+  const SWAGGER_DOCUMENTS = JSON.parse(fs.readFileSync(path.join("docs", "swagger.json")));
   const app = express();
 
   app.use(express.json());
   app.use(cookieParser());
   app.use(cors());
   app.use(pino());
+
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(SWAGGER_DOCUMENTS));
 
   app.use("/photos", express.static(path.resolve("src", "uploads", "photos")));
 
